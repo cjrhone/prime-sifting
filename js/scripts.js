09a9dba7967c3c07c41ad6ade0a50bea5acb282a
Alex Noble and CJ Rhone <cj@>
2018-03-27 16:21:29 -0700

    fix scope issues

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,12 @@ var index=0;
 var primeLoc=0;
 
 function resetVariables(){
-  var testArray=[1];
-  var limit=0;
-  var prime=2;
-  var finalPrimes=[1];
-  var index=0;
-  var primeLoc=0;
+  testArray=[1];
+  limit=0;
+  prime=2;
+  finalPrimes=[1];
+  index=0;
+  primeLoc=0;
 }
 
 function removeDivisible(){
@@ -27,13 +27,11 @@ function removeDivisible(){
 
 $(document).ready(function() {
 
-  //$("").text("");
   $("form#primeForm").submit(function(event) {
-
+    //$('ul').empty();
     resetVariables();//resets variables so that function doesn't double append
-    console.log(finalPrimes);
     //gets userInput for limit number
-    var limit = parseInt($("#inputBox").val());
+    limit = parseInt($("#inputBox").val());
 
     //build array that includes all numbers from 1 up to user inputted number
     for(var x=2; x<=limit; x++){
@@ -47,11 +45,13 @@ $(document).ready(function() {
     } while (prime); //loop while numbers exist in the test array (not including 1)
     finalPrimes.pop();//removes extra element
 
-    $(".result").append("<li>"+finalPrimes+ "</li>");
+    //$(".result").append("<li>"+finalPrimes+ "</li>");
+
+    $('ul').append('<li>' +finalPrimes+ '</li>');
 
     event.preventDefault();
 
-$(".result").fadeIn();
+    $(".result").fadeIn();
 
   })
 })
